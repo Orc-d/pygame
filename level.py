c665d2bf072f4790624c3b47dc9876d9f45808e5
ORCD <fkzns1102@knou.ac.kr>
2022-12-20 21:36:21 +0900

    Plater_Centering

--- level.py
+++ level.py
@@ -9,7 +9,9 @@ class Level:
         
         self.display_surface = pygame.display.get_surface()
         
-        self.visible_sprite = pygame.sprite.Group()
+        #카메라 설정을 한 그룹 ( 커스텀 메이드 )
+        self.visible_sprite = YSortCameraGroup()
+        
         self.obstacles_sprite = pygame.sprite.Group()
         
         self.create_map()
@@ -26,6 +28,29 @@ class Level:
                     self.player = Player((x,y),[self.visible_sprite],self.obstacles_sprite)
     
     def run(self):
-        self.visible_sprite.draw(self.display_surface)
+        self.visible_sprite.custom_draw(self.player)
         self.visible_sprite.update()
         debug(self.player.direction)
+ 
+#카메라 설정을 위해 Group 값을 상속 받고 커스텀        
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+    
+    def custom_draw(self,player):
+        
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        #offset 값을 줘서 플레이어를 가운데로
+        
+        for sprite in self.sprites():
+            #화면의 blit 메서드를 사용하여 Draw 처리 
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
+            # 여기까지가 draw 메서드랑 비슷 ( 드로우 메서드는 arg로 값을 줘야하는데 우린 미리 줌)
+            # 여기서 offset_pos 는 offset_rect 임
